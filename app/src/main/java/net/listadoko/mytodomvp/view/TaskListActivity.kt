e891d70be665690ac322fa8ab313f1290d4fdae0
ksugawara61 <katsuya@kokohadoko.net>
2020-05-04 15:00:22 +0900

    [add] first commit

---
+++ app/src/main/java/net/listadoko/mytodomvp/view/TaskListActivity.kt
@@ -0,0 +1,13 @@
+package net.listadoko.mytodomvp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.listadoko.mytodomvp.R
+
+class TaskListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_list)
+    }
+}
