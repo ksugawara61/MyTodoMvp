957f9bd8b163941ae0dc392a88ebd6898a286341
ksugawara61 <katsuya@kokohadoko.net>
2020-05-06 16:30:14 +0900

    [add] open drawer test

--- app/src/androidTest/java/net/listadoko/mytodomvp/view/TaskListActivityTest.kt
+++ app/src/androidTest/java/net/listadoko/mytodomvp/view/TaskListActivityTest.kt
@@ -1,10 +1,14 @@
 package net.listadoko.mytodomvp.view
 
 
+import android.content.Context
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import net.listadoko.mytodomvp.R
 import net.listadoko.mytodomvp.view.page.TaskListPage
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,15 +17,32 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TaskListActivityTest {
 
+    lateinit var context: Context
+
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(TaskListActivity::class.java)
 
+    @Before
+    fun setUp() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+
     @Test
-    fun taskListActivityTest() {
+    fun addTaskTest() {
         TaskListPage()
-            .assertHelloWorld("All TO-DOs")
+            .assertTitle("All TO-DOs")
             .goToTaskAdd()
             .assertTest("Add TO-DO")
+    }
+
+    @Test
+    fun openDrawerTest() {
+        TaskListPage()
+            .openNavigationDrawer()
+            .assertNavigationDrawerVisibility(true)
+            .closeNavigationDrawer()
+            .assertNavigationDrawerVisibility(false)
+            .assertTitle("All TO-DOs")
     }
 }
--- app/src/androidTest/java/net/listadoko/mytodomvp/view/page/TaskListPage.kt
+++ app/src/androidTest/java/net/listadoko/mytodomvp/view/page/TaskListPage.kt
@@ -1,10 +1,14 @@
 package net.listadoko.mytodomvp.view.page
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import net.listadoko.mytodomvp.R
+import net.listadoko.mytodomvp.view.page.PageUtil.childAtPosition
 import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
 
@@ -15,9 +19,57 @@ class TaskListPage {
         return TaskAddPage()
     }
 
-    fun assertHelloWorld(text: String): TaskListPage {
+    fun openNavigationDrawer(): TaskListPage {
+        val appCompatImageButton = onView(
+            allOf(
+                withContentDescription("Navigate up"),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.toolbar),
+                        childAtPosition(
+                            withId(R.id.app_bar_layout),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageButton.perform(click())
+        return this
+    }
+
+    fun closeNavigationDrawer(): TaskListPage {
+        val navigationMenuItemView = onView(
+            allOf(
+                childAtPosition(
+                    allOf(
+                        withId(R.id.design_navigation_view),
+                        childAtPosition(
+                            withId(R.id.nav_view),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        navigationMenuItemView.perform(ViewActions.click())
+        return TaskListPage()
+    }
+
+    fun assertTitle(text: String): TaskListPage {
         val textView = onView(allOf(withId(R.id.all_todo_text), isDisplayed()))
         textView.check(matches(withText(Matchers.equalTo(text))))
+        return this
+    }
+
+    fun assertNavigationDrawerVisibility(visible: Boolean): TaskListPage {
+        val imageButton = onView(withId(R.id.nav_view))
+        val matcher = if (visible) matches(isDisplayed()) else matches(Matchers.not(isDisplayed()))
+        imageButton.check(matcher)
         return this
     }
 }
